abe508782153a6603d11fbc6b26c0f3a13629149
vadim-sobinin <vadimsobinin@yandex.ru>
2023-02-03 14:51:12 +0300

    add slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import 'normalize.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './styles/main.scss';
 import './index.html';
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+const swiper = new Swiper('.offerSwiper', {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  modules: [Navigation, Pagination],
+});
